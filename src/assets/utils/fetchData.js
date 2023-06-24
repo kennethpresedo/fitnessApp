0c363b2df36fc0b38fd6a868b0fbe693fda20a00
Kenneth Presedo <kennethpresedo@live.com>
2023-06-24 19:46:43 -0400

    exercise video component complete also added youtube related videos api

--- src/assets/utils/fetchData.js
+++ src/assets/utils/fetchData.js
@@ -14,6 +14,14 @@ export const fetchData = async(url, options) => {
     return data;
 }
 
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+
 //This code snippet demonstrates an asynchronous function that 
 //makes an HTTP request, retrieves the response, parses it as JSON, 
 //and returns the resulting data.
